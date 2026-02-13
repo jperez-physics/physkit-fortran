b836f66d1200c4e9f72f6cdf320f99852747f485
jperez-physics <jaimeperez060507@gmail.com>
2026-02-12 23:05:15 -0500

    Verlet Method Added

--- src/physkit_ode.f90
+++ src/physkit_ode.f90
@@ -1,7 +1,7 @@
 module physkit_ode
     implicit none
     private
-    public :: euler, rk2, rk4
+    public :: euler, rk2, rk4, verlet
 
     interface
         function f(x, y)
@@ -113,5 +113,45 @@ contains
         ynext = y
 
     end subroutine rk4
+
+    !=================================================
+    ! Velocity Verlet Method for 2nd-order ODE
+    !=================================================
+    ! dx: step size
+    ! x0: initial independent variable (time) value
+    ! x1: final independent variable (time) value
+    ! yinitial: initial position at x0
+    ! vinitial: initial velocity at x0
+    ! ynext: output position at x1
+    ! vnext: output velocity at x1
+    ! yfunc: function that computes acceleration a(x, y) = d2y/dx2
+    ! NOTE: Here 'yfunc(x,y)' must return la acceleration (second order)
+    !=================================================
+    subroutine verlet(dx, x0, x1, yinitial, vinitial, ynext, vnext, yfunc)
+        real, intent(in)  :: dx, yinitial, vinitial, x0, x1
+        real, intent(out) :: ynext, vnext
+        procedure(f) :: yfunc
+
+        real :: y, v, x, a, anext
+        integer :: j, N
+
+        if (dx == 0.0) stop "Error: dx cannot be zero"
+        N = nint((x1 - x0) / dx)
+        y = yinitial
+        v = vinitial
+        x = x0
+
+        do j = 1, N
+            a = yfunc(x, y)
+            y = y + v*dx + 0.5*a*dx*dx
+            anext = yfunc(x + dx, y)
+            v = v + 0.5*(a + anext)*dx
+            x = x + dx
+        end do
+
+        ynext = y
+        vnext = v
+
+    end subroutine verlet
 
 end module physkit_ode
